90cd6d61a9bca4e99c13f3157c984e5b8bdbdd0c
andres10afg <andres10afg@gmail.com>
2026-09-01 01:36:57 -0400

    Cambiar espacio en blanco por medio avance de papel (ESC J) entre Total y Propina sugerida

--- script.js
+++ script.js
@@ -398,8 +398,9 @@ function imprimirConRawBT() {
   });
   t += FUENTE_A;
   t += separador;
+  const MEDIO_ESPACIO = '\x1B\x4A\x0C';
   t += `${BOLD_ON}${fila('Total', formatoMoneda(r.total))}${BOLD_OFF}`;
-  t += '\n';
+  t += MEDIO_ESPACIO;
   t += fila('Propina sugerida (10%)', formatoMoneda(r.propina));
   t += fila('Total con propina', formatoMoneda(r.total + r.propina));
   t += separador;
